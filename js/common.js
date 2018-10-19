a2420b97a1bcf66e56cfb15f311a27cbbcc84639
Sergey Skripnyuk <nightskyles@gmail.com>
2018-10-19 19:48:59 +0300

    optimized toggler

--- js/common.js
+++ js/common.js
@@ -1,52 +1,10 @@
 "use strict"
 
-function toggleButton1() {
-	let tab1 = document.getElementById('tab-button1');
-	let tab2 = document.getElementById('tab-button2');
-	let tab3 = document.getElementById('tab-button3');
-	if ( !(tab1.classList.contains('selected')) ) {
-		tab1.classList.toggle('selected');
-	};
-	if ( (tab2.classList.contains('selected')) ) {
-		tab2.classList.toggle('selected');
-	};
-	if ( (tab3.classList.contains('selected')) ) {
-		tab3.classList.toggle('selected');
-	};
-	
-}
-
-function toggleButton2() {
-	let tab1 = document.getElementById('tab-button1');
-	let tab2 = document.getElementById('tab-button2');
-	let tab3 = document.getElementById('tab-button3');
-	if ( (tab1.classList.contains('selected')) ) {
-		tab1.classList.toggle('selected');
-	};
-	if ( !(tab2.classList.contains('selected')) ) {
-		tab2.classList.toggle('selected');
-	};
-	if ( (tab3.classList.contains('selected')) ) {
-		tab3.classList.toggle('selected');
-	};
-	
-}
-
-
-function toggleButton3() {
-	let tab1 = document.getElementById('tab-button1');
-	let tab2 = document.getElementById('tab-button2');
-	let tab3 = document.getElementById('tab-button3');
-	if ( (tab1.classList.contains('selected')) ) {
-		tab1.classList.toggle('selected');
-	};
-	if ( (tab2.classList.contains('selected')) ) {
-		tab2.classList.toggle('selected');
-	};
-	if ( !(tab3.classList.contains('selected')) ) {
-		tab3.classList.toggle('selected');
-	};
-	
-}
-
-
+let buttons = document.getElementsByClassName('buttons');
+let toggleWrapper = document.querySelector('.tabs-toggle')
+toggleWrapper.onclick = function(e) {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].classList.remove('selected');
+	}
+	e.target.classList.add('selected');
+};
